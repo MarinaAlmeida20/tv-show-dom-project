2a5869f1d1243b7808f2fa98f24246dbb478e116
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-02-20 20:04:50 +0000

    Level 100 done

--- script.js
+++ script.js
@@ -6,7 +6,33 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((episode) => {
+    // // create Elements
+    const divElement = document.createElement("div");
+
+    const headingElem = document.createElement("h3");
+    const imageElem = document.createElement("img");
+    const summaryElem = document.createElement("p");
+
+    // set the class to the Elements to use in CSS
+    divElement.setAttribute("class", "content");
+    headingElem.setAttribute("class", "heading");
+    imageElem.setAttribute("class", "image");
+
+    // set Elements
+    headingElem.innerHTML = `${episode.name} - S0${episode.season}E0${episode.number}`;
+
+    imageElem.src = `${episode.image.medium}`;
+
+    summaryElem.innerHTML = `${episode.summary}`;
+
+    // append Element to the page
+    divElement.append(headingElem, imageElem, summaryElem);
+
+    rootElem.append(divElement);
+  });
 }
 
 window.onload = setup;
