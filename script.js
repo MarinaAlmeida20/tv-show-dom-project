f4bddd3364b427cd4e282cf119248a7e3c2d3d1b
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-03-11 17:57:01 +0000

    fixed the option empty space

--- script.js
+++ script.js
@@ -14,49 +14,34 @@ let allCount;
 
 let currentEpisodes = [];
 
+searchBar.addEventListener("keyup", onSearchKeyUp);
+selectEpisodes.addEventListener("change", episodeSelected);
+selectShow.addEventListener("change", showSelected);
+
 // setUp
 function setup() {
-  sendRequest(1).then((data) => {
+  let allShows = getAllShows();
+  sendRequest(167).then((data) => {
     currentEpisodes = data;
     makePageForEpisodes(currentEpisodes);
   });
-  nameShow();
-  searchBar.addEventListener("keyup", onSearchKeyUp);
-  selectEpisodes.addEventListener("change", episodeSelected);
+  makeSelectMenuForShows(allShows);
 }
 
-// show all the episodes
-function makePageForEpisodes(episodeList) {
-  const ulElem = document.getElementById("episodesList");
-  ulElem.innerHTML = "";
-  const optionEpisodes = document.createElement("option");
+// fetch episodes and shows
+function sendRequest(showId) {
+  const urlForTheRequest = `https://api.tvmaze.com/shows/${showId}/episodes`;
+  const urlForTheCast = `https://api.tvmaze.com/shows/${showId}?embed=cast`;
+  // console.log(showId);
 
-  episodeList.forEach((episode) => {
-    const li = document.createElement("li");
-    li.setAttribute("class", "content");
-    let episodeNumber = episode.number.toString().padStart(2, "0");
-    li.innerHTML = `
-    <h3 class="heading">${episode.name} - S0${episode.season}E${episodeNumber}</h3>
-    <img class="image" src="${episode.image.medium}" />
-    <p>${episode.summary}</p>
-    `;
-    ulElem.appendChild(li);
-  });
+  fetch(urlForTheCast)
+    .then((res) => res.json())
+    .then((data) => data);
 
-  if (episodeList.length > 1) {
-    selectEpisodes.innerHTML = "";
-    // add the option in select
-    episodeList.filter((e) => {
-      let optionElementReference = new Option(
-        `${e.name} - S0${e.season}E${e.number.toString().padStart(2, "0")}`,
-        `${e.name}`
-      );
-
-      selectEpisodes.add(optionElementReference);
-    });
-    selectEpisodes.appendChild(optionEpisodes);
-    console.log(selectEpisodes);
-  }
+  return fetch(urlForTheRequest)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.log(err));
 }
 
 // search
@@ -73,16 +58,11 @@ function onSearchKeyUp(event) {
   filteredCount = filteredEpisodes.length;
   allCount = currentEpisodes.length;
 
-  displayingTotal();
+  totalEpisodesDisplayed.innerHTML = `Displaying ${filteredCount}/${allCount} episode(s)`;
   makePageForEpisodes(filteredEpisodes);
 }
 
-// display the total number of episodes
-function displayingTotal() {
-  totalEpisodesDisplayed.innerHTML = `Displaying ${filteredCount}/${allCount} episode(s)`;
-}
-
-// episode selected, eventlistener
+// target the episode in select
 function episodeSelected(e) {
   searchEpisode = e.target?.value;
   let arr = [];
@@ -95,47 +75,77 @@ function episodeSelected(e) {
   });
 }
 
-// fetch episodes
-function sendRequest(showId) {
-  const urlForTheRequest = `https://api.tvmaze.com/shows/${showId}/episodes`;
-  // console.log(showId);
-
-  return fetch(urlForTheRequest)
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((err) => console.log(err));
-}
-
-// show witch id selected
-
-selectShow.addEventListener("click", function showSelected(e) {
+// target the show in select
+function showSelected(e) {
   searchShow = e.target.value;
-  sendRequest(searchShow).then((anything) => {
-    currentEpisodes = anything;
+  sendRequest(searchShow).then((showIdSelected) => {
+    currentEpisodes = showIdSelected;
     makePageForEpisodes(currentEpisodes);
   });
-});
+}
 
-// console.log(idShows);
-// created the select shows and fetch shows
-function nameShow() {
-  const urlNameShow = `https://api.tvmaze.com/shows`;
-  fetch(urlNameShow)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.length > 1) {
-        const optionShow = document.createElement("option");
-        data.forEach((e) => {
-          const nameShown = e.name;
-          const idShown = e.id;
-          let optionElementReference = new Option(nameShown, idShown);
-          selectShow.add(optionElementReference);
-        });
-        selectShow.appendChild(optionShow);
-        // console.log(selectShow);
-      }
-    })
-    .catch((err) => console.log(err));
+// show all the episodes
+function makePageForEpisodes(episodeList) {
+  const ulElem = document.getElementById("episodesList");
+  ulElem.innerHTML = "";
+  let optionEpisodes = document.createElement("option");
+
+  episodeList.forEach((episode) => {
+    const li = document.createElement("li");
+    let episodeNumber = episode.number.toString().padStart(2, "0");
+    let episodeSeason = episode.season.toString().padStart(2, "0");
+    li.setAttribute("class", "content");
+    // console.log(episodeSesion);
+    li.innerHTML = `
+    <h3 class="heading">${episode.name} - S${episodeSeason}E${episodeNumber}</h3>
+    <img class="image" src="${episode.image.medium}" />
+    <p>${episode.summary}</p>
+    `;
+    // console.log(li);
+    ulElem.appendChild(li);
+  });
+
+  if (episodeList.length > 0) {
+    selectEpisodes.innerHTML = "";
+    // add the option in select
+    episodeList.filter((e) => {
+      let episodeNumber = e.number.toString().padStart(2, "0");
+      let episodeSeason = e.season.toString().padStart(2, "0");
+      optionEpisodes = new Option(
+        `${e.name} - S${episodeSeason}E${episodeNumber}`,
+        `${e.name}`
+      );
+
+      selectEpisodes.add(optionEpisodes);
+    });
+    selectEpisodes.appendChild(optionEpisodes);
+  }
+}
+
+// created the select shows in alphabetical order
+function makeSelectMenuForShows(shows) {
+  shows.sort((showA, showB) => {
+    const { name: nameA } = showA;
+    const { name: nameB } = showB;
+
+    if (nameA.toLowerCase() < nameB.toLowerCase()) {
+      return -1;
+    } else if (nameA.toLowerCase() > nameB.toLowerCase()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  let optionShow = document.createElement("option");
+
+  shows.forEach((e) => {
+    const nameShown = e.name;
+    const idShown = e.id;
+    optionShow = new Option(nameShown, idShown);
+    selectShow.add(optionShow);
+  });
+  selectShow.appendChild(optionShow);
 }
 
 window.onload = setup;
